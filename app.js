04f3b1602bf8550f153096ab8c0740b449e5450a
Mayur Kapse <mayurkapse785@gmail.com>
2026-09-13 02:01:50 +0530

    Fix root route redirect

--- app.js
+++ app.js
@@ -96,6 +96,10 @@ app.use("/listings",listingRouter);
 app.use("/listings/:id/reviews",ReviewRouter);
 app.use("/",userRouter);
 
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
+
 
 app.use((req, res, next) => {
     next(new ExpressError(404, "Page not found"));
